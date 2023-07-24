845f271b9860550eeb1938904ebbfe040cc4df83
Ruzell03 <ruzell@matchmo.ph>
2023-07-24 11:29:54 +0800

    add logic if its in production or development

--- src/modules/common/constants.js
+++ src/modules/common/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://143.198.75.195:3000/api";
+const DEVELOPMENT_ENV = "development"
+export const API_URL = import.meta.env.VITE_SERVER === DEVELOPMENT_ENV ? "http://localhost:3000/api" : "http://143.198.75.195:3000/api";
